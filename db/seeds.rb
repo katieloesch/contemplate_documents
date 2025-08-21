a983970254f4ceb047cef5d5a29ee12a1da942f2
Katie Loesch <catherine.loesch@protonmail.com>
2025-08-21 21:04:15 +0100

    display documents in descending order by created_at property

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-10.times do |d|
-  ContemplateDocument.create!(
-    title: "Document #{d}",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis semper tristique. Integer ut metus lacinia, cursus justo id, sodales velit. Sed tincidunt, felis sed.",
-    file_url: 'https://drive.google.com/file/d/1WJyjAfRmdgFbZq-ii5HKGQepjDMFpixf/view?usp=sharing',
-    image_url: "https://cdn.pixabay.com/photo/2014/08/26/19/21/document-428335_1280.jpg",
-  )
-end 
+# 10.times do |d|
+#   ContemplateDocument.create!(
+#     title: "Document #{d}",
+#     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis semper tristique. Integer ut metus lacinia, cursus justo id, sodales velit. Sed tincidunt, felis sed.",
+#     file_url: 'https://drive.google.com/file/d/1WJyjAfRmdgFbZq-ii5HKGQepjDMFpixf/view?usp=sharing',
+#     image_url: "https://cdn.pixabay.com/photo/2014/08/26/19/21/document-428335_1280.jpg",
+#   )
+# end 
+
+# ContemplateDocument.create!(title: "Another Document", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis semper tristique. Integer ut metus lacinia, cursus justo id, sodales velit. Sed tincidunt, felis sed.", file_url: 'https://drive.google.com/file/d/1WJyjAfRmdgFbZq-ii5HKGQepjDMFpixf/view?usp=sharing', image_url: "https://cdn.pixabay.com/photo/2024/11/18/10/20/businessman-9205819_1280.png")
